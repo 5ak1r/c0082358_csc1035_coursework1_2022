e657b909ec083205a6fc9bcba7357cc0a3717d66
5ak1r <s.azimkar1@newcastle.ac.uk>
2023-02-20 22:15:13 +0000

    Created psvm and main menu methods

--- CSC1035_proj1/ReportingIO.java
+++ CSC1035_proj1/ReportingIO.java
@@ -1,4 +1,41 @@
 package CSC1035_proj1;
 
+import java.util.Scanner;
+
+
 public class ReportingIO {
+
+    public static void main() {
+        while(true) {
+            mainMenu();
+        }
+    }
+
+    public static void mainMenu() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Main Menu\nSelect an option using the corresponding number:\n" +
+                "1. Enter AuctionHouse data\n" +
+                "2. Enter Item data\n" +
+                "3. Provide Reporting Statistics\n" +
+                "4. Exit");
+        Character choice = scanner.nextLine().charAt(0);
+
+        switch(choice) {
+            case 1:
+                auctionHouse();
+                break;
+            case 2:
+                item();
+                break;
+            case 3:
+                reporting();
+                break;
+            case 4:
+                System.exit(0);
+                break;
+            default:
+                throw new IllegalArgumentException("That is not a valid option");
+
+        }
+    }
 }
