b19e52886306d15b1575c1c5852cc9cb0da754e8
5ak1r <s.azimkar1@newcastle.ac.uk>
2023-02-22 01:37:18 +0000

    Bug fixing; Reporting.java now informs user if no Item.java or AuctionHouse.java exist.

--- src/CSC1035_proj1/Reporting.java
+++ src/CSC1035_proj1/Reporting.java
@@ -21,7 +21,7 @@ public class Reporting {
     }
 
     //Adds a newly created auction house to the Reporting class
-    public void addAuctionHouse(AuctionHouse ah) throws Exception {
+    public void addAuctionHouse(AuctionHouse ah) {
         ArrayList<AuctionHouse> temp_list = new ArrayList<>(getAuctionHouses());
         temp_list.add(ah);
         setAuctionHouses(temp_list);
@@ -30,20 +30,21 @@ public class Reporting {
     //Returns the Item with the highest price ever recorded
     public Item mostExpensive() throws Exception {
         //Temporary Item with 0 price to begin comparing
-        Item highest_price = new Item(199999, "",0,2000,"");
+        Item highest_price = new Item(199999, "No Items", (float) 0, 2000, "f");
 
-        for (AuctionHouse ahs: getAuctionHouses()) {
-            if(ahs.highestPrice().getPriceSold() > highest_price.getPriceSold()) {
+        for (AuctionHouse ahs : getAuctionHouses()) {
+            if (ahs.highestPrice().getPriceSold() > highest_price.getPriceSold()) {
                 highest_price = ahs.highestPrice();
             }
         }
+
         return highest_price;
     }
 
     //Returns the auction house with the largest average price for a given year
     public AuctionHouse highestAverage(int year) {
         //Temporary empty AuctionHouse to begin comparing
-        AuctionHouse highest_average = new AuctionHouse("temp");
+        AuctionHouse highest_average = new AuctionHouse("No Auction Houses have been added yet");
 
         for (AuctionHouse ahs: getAuctionHouses()) {
             if(ahs.averagePrice(year) > highest_average.averagePrice(year)) {
@@ -61,6 +62,7 @@ public class Reporting {
         for (AuctionHouse ahs: getAuctionHouses()) {
             total_arraylist.addAll(ahs.priceGreater(price));
         }
+
         return total_arraylist;
     }
 }
